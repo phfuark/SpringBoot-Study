34d4d202db553f3bec6dd5705660e43dab5c2b49
phfuark <paulohsilva2007@gmail.com>
2025-01-13 18:29:24 -0300

    13.01.2025 - Criando Método 'void' Delete

--- api/src/main/java/com/study/api/controller/Controller.java
+++ api/src/main/java/com/study/api/controller/Controller.java
@@ -4,6 +4,7 @@ package com.study.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,13 @@ public class Controller {
     @PutMapping("api")
     public Person edit(@RequestBody Person obj){
         return action.save(obj);
+    }
+
+    @DeleteMapping("/api")
+    public void remove(@PathVariable int code){
+        Person obj = selectByCode(code);
+
+        action.delete(obj);
     }
 
     @GetMapping("")
