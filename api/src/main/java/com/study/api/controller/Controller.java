75eea2bfdefe7c030b060f7e0b6d64161e31720a
phfuark <paulohsilva2007@gmail.com>
2025-01-15 20:52:08 -0300

    15.01.2024 - ResponseEntity(HttpStatus)

--- api/src/main/java/com/study/api/controller/Controller.java
+++ api/src/main/java/com/study/api/controller/Controller.java
@@ -4,6 +4,8 @@ package com.study.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -89,6 +91,12 @@ public class Controller {
     public List<Person> ageHighEquals(){
         return action.ageHigEquals(20);
     }
+
+    @GetMapping("/status")
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 
     @GetMapping("")
     public String HelloWord(){
