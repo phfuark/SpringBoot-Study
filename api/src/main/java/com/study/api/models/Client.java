f2edb4252fa3eeb89aa200e2a375b7b888e52e9b
phfuark <paulohsilva2007@gmail.com>
2025-01-16 19:46:18 -0300

    16.01.2025 - Validando através de JPA

---
+++ api/src/main/java/com/study/api/models/Client.java
@@ -0,0 +1,40 @@
+package com.study.api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int code;
+    private String name;
+    private String email;
+
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
